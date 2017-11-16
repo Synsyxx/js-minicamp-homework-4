00c45e4000404cc070d01cefa03e3c6de633de4e
Synsyxx <synsyxx@github.com>
2017-11-16 17:51:04 -0600

    Complete Precourse Work 4

--- exercises.js
+++ exercises.js
@@ -1,30 +1,62 @@
 // Do not change any of the function names
+/*Callback Functions - These allow you to define a function to use later,
+so if you think of something awesome that you don't need just yet,
+you get to save it for later!
+Closure - an inner function that has access to everything outside of it, including
+the function enclosing it and global variables.
+Arguments - A piece of info that you pass through a function.
+Recursion - This is a concept. This is when a function calls itself.
+Prototype - This refers to the categories of objects. These of course depend on
+how the object is made.
+Constructors - These are functions that make objects.
+*/
 
 function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+  var product = 1;
+  if (arguments.length === 0){
+    return 0;
+  }
+  for (var i = 0; i < arguments.length; i++){
+    product *= arguments[i];
+  }
+  return product;
 }
 
 function invokeCallback(cb) {
   // invoke cb
+  cb();
 }
 
 function sumArray(numbers, cb) {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+  var sum = 0;
+  for (var i = 0; i < numbers.length; i++){
+    sum += numbers[i];
+  }
+  cb(sum);
 }
 
 function forEach(arr, cb) {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+  arr.forEach(function(x){
+    cb(x);
+  });
 }
 
 function map(arr, cb) {
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
+  var array = arr.map(function(x){
+    return cb(x);
+  });
+  return array;
 }
 
 function getUserConstructor() {
@@ -34,11 +66,23 @@ function getUserConstructor() {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the constructor
+  return function (options){
+    this.username = options.username;
+    this.name = options.name;
+    this.email = options.email;
+    this.password = options.password;
+    this.sayHi = function (){
+      return 'Hello, my name is' + this.name;
+    };
+  };
 }
 
 function addPrototypeMethod(Constructor) {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+  Constructor.prototype.sayHi = function () {
+    return 'Hello World!';
+  };
 }
 
 function addReverseString() {
@@ -46,6 +90,13 @@ function addReverseString() {
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
+  String.prototype.reverse = function () {
+    var newString = '';
+    for (var i =this.length -1; i > -1; i--){
+      newString += this[i];
+    }
+    return newString;
+  };
 }
 
 function nFactorial(n) {
@@ -53,6 +104,8 @@ function nFactorial(n) {
   // solve this recursively
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+  if (n === 0) return 1;
+  return n * nFactorial(n-1);
 }
 
 function cacheFunction(cb) {
@@ -67,6 +120,16 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
+  var cache = {};
+  return function(arg){
+    if(cache.hasOwnProperty(arg)){
+      return cache[arg];
+    }
+    else {
+      cache[arg] = cb(arg);
+      return cache[arg];
+    }
+  };
 }
 
 
